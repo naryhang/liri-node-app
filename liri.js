e5b19c21ac4ae438d0fe053b87b310c97954131d
Nary Hang <naryhang@gmail.com>
2019-08-02 18:26:20 -0700

    updating variable for spotify

--- liri.js
+++ liri.js
@@ -37,8 +37,9 @@ function userRequest(command, param) {
 
         // If no command is entered, this is the default message to user
         default:
-        console.log('Please enter a valid command.');
-    }    
+        console.log('Please enter a valid command: spotify-this-song, concert-this, movie-this, or do-what-it-says.');
+    }
+    
 }
 
 //1. spotify-this
@@ -46,9 +47,10 @@ function spotifyThis(song) {
     if (!song) {
         song = 'The Sign by Ace of Base';
     }
-    spotify.search ({ type: 'track', query: song}, function (err, data){
+    spotify.search({ type: 'track', query: song}, function (err, data){
         if (err){
-            return console.log('Error occured: ' + err);          
+            return console.log('Error occured: ' + err);
+            
         }
         else{
             var tracks = data.tracks.items;
@@ -57,11 +59,12 @@ function spotifyThis(song) {
                 var artistArr = tracks[i].artists;
                 var artists = [];
                 for (var j = 0; j < artistArr.length; j++){
-                    artists.push(artistArr[j].name);               
+                    artists.push(artistArr[j].name);
+                    
                 }
                console.log('Artist: ' + artists.join(", "));
-               console.log('Song: ' + tracks[i].name);
-               console.log('Album: ' + tracks[i].album.name);
+                console.log('Song: ' + tracks[i].name);
+                console.log('Album: ' + tracks[i].album.name);
 
                 if (tracks[i].preview_url == null) {
                     console.log ('Preview unavailable');
@@ -70,7 +73,8 @@ function spotifyThis(song) {
                     console.log('Preview: ' + tracks[i].preview_url);
                 }
                 console.log("------------------------------")
-            }                
+            }
+                
         }
     });
 }
@@ -78,12 +82,13 @@ function spotifyThis(song) {
 //2. concert-this
 function concertThis(artist) {
     var artistQ = artist.split(' ').join('+');
+
     var queryURL = "https://rest.bandsintown.com/artists/" + artistQ + "/events?app_id=" + keys.bandsintown;
     request(queryURL, function(err, response, body) {
         if (err) {
             console.log(err);
             return;
-	}
+        }
         else{
             var result = JSON.parse(body);
             console.log(result);
@@ -103,24 +108,29 @@ function movieThis(movie){
     if(!movie) {
        movie = "Mr. Nobody";
     }
+
     let queryURL = 'http://www.omdbapi.com/?t=' + movie +'&y=&plot=long&tomatoes=true&r=json&apikey=trilogy'
+
     let movieQ = movie.split(' ').join('+');
+
     request(queryURL, function(error, response, body) {
+
         if(error) {
             console.log(error);
         } else {
             let result = JSON.parse(body);
             console.log("------------------------------")
             console.log("Title: " + result["Title"]);
-	    console.log("Year: " + result["Year"]);
+	        console.log("Year: " + result["Year"]);
             console.log("IMDB Rating: " + result["imdbRating"]);
             if ( result.Ratings[1]) {
-            console.log("Rotten Tomatoes Rating: " + result.Ratings[1].Value);
-            }        
-	    console.log("Country: " + result["Country"]);
-	    console.log("Language: " + result["Language"]);
-	    console.log("Plot: " + result["Plot"]);
-	    console.log("Actors: " + result["Actors"]);
+                console.log("Rotten Tomatoes Rating: " + result.Ratings[1].Value);
+            }
+            
+	        console.log("Country: " + result["Country"]);
+	        console.log("Language: " + result["Language"]);
+	        console.log("Plot: " + result["Plot"]);
+	        console.log("Actors: " + result["Actors"]);
             console.log("Rotten Tomatoes URL: " + result["tomatoURL"])
             console.log("------------------------------")
     }
@@ -131,14 +141,18 @@ function movieThis(movie){
 function getRandom() {
     fs.readFile("random.txt", "utf8", function(error, data) {
         if(error){
-        console.log(error);
+            console.log(error);
         }else {
             var dataSplit = data.split(',');
             var readCommand = dataSplit[0];
             var readParam = dataSplit[1];
             
+        
+
             userRequest(readCommand, readParam);
         }
     })
 }
+
+
  userRequest(inputCommand, inputParam);
